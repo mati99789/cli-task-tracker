7c838d6fef30f405d59b0ebb4dfe40ea87f3e56e
Mateusz Urbaniak <matteus.urbaniak@hotmail.com>
2024-10-23 17:23:32 +0200

    Feat: Basic storage struct.

--- internal/storage/json-storage.go
+++ internal/storage/json-storage.go
@@ -1,1 +1,19 @@
 package storage
+
+import (
+	"cli-task-tracker/internal/models"
+	"errors"
+)
+
+type JsonStorage struct {
+	Tasks map[string]*models.Task
+}
+
+func NewJsonStorage(filePath string) (*JsonStorage, error) {
+
+	if filePath == "" {
+		return nil, errors.New("filePath is empty")
+	}
+
+	return &JsonStorage{}, nil
+}
