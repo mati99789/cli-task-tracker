bb22b3709a23e10577fbaf64557c5e8a952329d7
Mateusz Urbaniak <matteus.urbaniak@hotmail.com>
2024-10-23 19:09:14 +0200

    Feat: Add loadTasks func from json if exists.

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,16 @@
-package cmd
+package main
+
+import (
+	"cli-task-tracker/internal/storage"
+	"log"
+)
 
 func main() {
+
+	_, err := storage.NewJsonStorage("tasks.json")
+
+	if err != nil {
+		log.Fatalf("Failed to initialize storage: %v", err)
+	}
 
 }
--- internal/storage/json-storage.go
+++ internal/storage/json-storage.go
@@ -2,12 +2,18 @@ package storage
 
 import (
 	"cli-task-tracker/internal/models"
+	"encoding/json"
 	"errors"
+	"os"
 )
 
 var (
-	ErrEmptyFilePath = errors.New("file path is empty")
-	ErrTaskNotFound  = errors.New("task not found")
+	ErrEmptyFilePath  = errors.New("file path is empty")
+	ErrTaskNotFound   = errors.New("task not found")
+	ErrFileNotFound   = errors.New("file not found")
+	ErrFileCreated    = errors.New("file cant be created")
+	ErrCantLoadFile   = errors.New("can't load file")
+	ErrCantDecodeFile = errors.New("can't decode file")
 )
 
 type JsonStorage struct {
@@ -26,5 +32,61 @@ func NewJsonStorage(filePath string) (*JsonStorage, error) {
 		Tasks:    make(map[string]*models.Task),
 	}
 
+	_, err := os.Stat(filePath)
+
+	if os.IsNotExist(err) {
+		if err := createFile(filePath); err != nil {
+			return nil, err
+		}
+	} else if err != nil {
+		return nil, ErrFileCreated
+	}
+
+	if err := storage.LoadTask(); err != nil {
+		return nil, ErrCantLoadFile
+	}
+
 	return storage, nil
+}
+
+func (storage *JsonStorage) LoadTask() error {
+	file, err := os.Open(storage.filePath)
+	if err != nil {
+		return ErrCantLoadFile
+	}
+
+	defer file.Close()
+
+	fileInfo, err := file.Stat()
+
+	if err != nil {
+		createFile(storage.filePath)
+	}
+
+	if fileInfo.Size() == 0 {
+		storage.Tasks = make(map[string]*models.Task)
+		return nil
+	}
+
+	decoder := json.NewDecoder(file)
+
+	var tasks = make(map[string]*models.Task)
+	if err := decoder.Decode(&storage.Tasks); err != nil {
+		return ErrCantDecodeFile
+	}
+
+	storage.Tasks = tasks
+	return nil
+
+}
+
+func createFile(filePath string) error {
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	return nil
 }
